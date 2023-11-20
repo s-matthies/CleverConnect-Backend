8de7f2ad3768a77b691d97585dd0aaa7f2fa1b24
Julia Neubert <julia.neubert@student.htw-berlin.de>
2023-11-20 17:45:03 +0100

    Löschen UserEntity, Änderung Role zu enum, Hinzufügen enum Role zu Student

--- api/src/main/java/com/example/api/Entitys/Role.java
+++ api/src/main/java/com/example/api/Entitys/Role.java
@@ -1,23 +1,7 @@
 package com.example.api.Entitys;
 
-import jakarta.persistence.*;
-
-@Entity
-@Table(name = "Rolle")
-public class Role {
-
-    @Id // Schlüsselattribut der Entität Role ist id
-    @GeneratedValue(strategy = GenerationType.IDENTITY) // Wert wird automatisch generiert und inkrementiert
-    private Long id;
-
-    private String name;
-
-    public Long getId() { return this.id; }
-
-    public void setId(Long id) { this.id = id; }
-
-    public String getName() { return this.name; }
-
-    public void setName(String name) { this.name = name; }
-
+public enum Role {
+    STUDENT,
+    EXTERN,
+    ADMIN
 }
--- api/src/main/java/com/example/api/Entitys/Student.java
+++ api/src/main/java/com/example/api/Entitys/Student.java
@@ -1,11 +1,17 @@
 package com.example.api.Entitys;
 
 import jakarta.persistence.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
 
 
 @Entity
 @Table(name="Student")
-public class Student {
+public class Student implements UserDetails {
 
     //Schlüsselattribut Id vergeben
     @Id
@@ -14,11 +20,12 @@ public class Student {
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "studentSequence")
 
     private Long id;
-
     private String firstName;
     private String lastName;
     private String email;
     private String password;
+    @Enumerated(EnumType.STRING) // sagt Spring, dass role ein enum ist und gibt diese als String wieder
+    private Role role;
 
     public Student() {
     }
@@ -71,6 +78,7 @@ public class Student {
         this.email = email;
     }
 
+    @Override
     public String getPassword() {
         return password;
     }
@@ -78,4 +86,35 @@ public class Student {
     public void setPassword(String password) {
         this.password = password;
     }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.name()));
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
 }
