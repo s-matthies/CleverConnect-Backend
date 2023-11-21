f3d7c7def67e89ae96961069ed129acbd7be85af
Sabine Matthies <Sabine.Matthies@student.htw-berlin.de>
2023-11-21 21:20:46 +0100

    ExternalConfig-Klasse hinzugefuegt um DB zu befuellen / Testen

---
+++ api/src/main/java/com/example/api/Config/ExternalConfig.java
@@ -0,0 +1,41 @@
+package com.example.api.Config;
+
+import com.example.api.Entitys.External;
+import com.example.api.Repository.ExternalRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+public class ExternalConfig {
+    @Autowired
+    //Configuration mit Repository verknüpfen
+    private final ExternalRepository externalRepository;
+
+    public ExternalConfig(ExternalRepository externalRepository) {
+        this.externalRepository = externalRepository;
+    }
+
+    //wird beim Start des Programms ausgeführt
+    @Bean
+    CommandLineRunner initDataExtern(){
+        return args -> {
+            External external1 = new External(
+                    "Missy",
+                    "Elliot",
+                    "missy.elliot@mail.de",
+                    "qwertz",
+                    "Big Data Company",
+                    "01/2024-12/2024"
+            );
+
+            externalRepository.saveAll(
+                    List.of(external1)
+            );
+        };
+    }
+
+}
