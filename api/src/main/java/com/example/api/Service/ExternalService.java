3ebe3ea520b3b0c94a5876d2199e1a9cc331c3db
Sabine Matthies <Sabine.Matthies@student.htw-berlin.de>
2024-01-06 12:08:11 +0100

    Fix Registration External
    bei Postman getestet - funktioniert; Löschen und Login External funktioniert nicht

--- api/src/main/java/com/example/api/Service/ExternalService.java
+++ api/src/main/java/com/example/api/Service/ExternalService.java
@@ -40,18 +40,31 @@ public class ExternalService {
         this.bCryptPasswordEncoder = bCryptPasswordEncoder;
     }
 
-    /*
-     * User (hier Extern) soll sich mit seiner E-Mail-Adresse einloggen können.
-     * Dafür muss die gewährleistet werden, dass die E-Mail-Adresse eindeutig ist.
-     * Wir checken, ob in unserer DB die eingegebene E-Mail bereits vorhanden ist.
-     * Über das Repository können wir unsere DB nach bestimmten Usern durchsuchen.
+    /**
+     * Methode für die Regostrierung einer externen Person (Zweitbtreuer*in)
+     * @param externalRequest Die Anfrage mit den Daten der zu registrierenden Person (Zweitbetreuer*in)
+     * @return  ResponseEntity mit den Daten der registrierten Person (Zweitbetreuer*in)
+     * @throws IllegalStateException wenn die E-Mail bereits vergeben ist
      */
+    public ResponseEntity<?> registration(ExternalRequest externalRequest) {
+        try{
+            External external = new External(
+                    externalRequest.getFirstName(),
+                    externalRequest.getLastName(),
+                    externalRequest.getEmail(),
+                    externalRequest.getPassword(),
+                    null,
+                    Role.EXTERN,
+                    false,
+                    true,
+                    externalRequest.getCompany(),
+                    externalRequest.getAvailabilityStart(),
+                    externalRequest.getAvailabilityEnd(),
+                    externalRequest.getDescription(),
+                    null  // Set BachelorSubjects to null initially
+            );
 
-    //SignUp-Methode erstellen
-    public ResponseEntity<?> registerExtern(External external) {
-        try {
             //existiert student mit dieser email bereits in der DB?
-            // im Repository findByMethode angelegt (-> E-Mail-Adresse muss eindeutig sein)
             boolean externExists = externalRepository.findByEmail(external.getEmail()).isPresent();
 
             //wenn E-Mail bereits existiert, dann Exception
@@ -60,92 +73,36 @@ public class ExternalService {
             }
 
             // Passwort verschlüsseln
-            String encodedPassword = bCryptPasswordEncoder
-                    .encode(external.getPassword());
-
+            String encodedPassword = bCryptPasswordEncoder.encode(external.getPassword());
             external.setPassword(encodedPassword);
 
             // das Registrierungsdatum auf das aktuelle Datum setzen
             external.setRegistrationDate(LocalDate.now());
 
-            User savedUser = externalRepository.save(external);
+            External savedExternal = externalRepository.save(external);
 
             emailService.sendEmail(external.getEmail(),
                     "Willkommen im System",
-                    "Hallo, Sie haben sich erfolgreich registriert und können die Platform nun nutzen. Viel Freude dabei!");
+                    "Hallo, Sie haben sich erfolgreich registriert und können die Plattform nun nutzen. " +
+                            "Viel Freude dabei!");
 
-
-            return ResponseEntity.ok(savedUser);
-
-            //String message = "{\"User*in wurde erfolgreich registriert!\"}";
-            //return ResponseEntity.ok(message);
-        }
-        catch (IllegalStateException e) {
-            // Exception abfangen und Fehler-JSON-Response zurückgeben
-            String errorMessage = "{\"error\": \"" + e.getMessage() + "\"}";
-            return ResponseEntity.badRequest().body(errorMessage);
-        }
-    }
-
-    /*
-     * wir erstellen eine Request: das was von uns an den Server weitergeben
-     * werden soll, um eine Userin (Externe) zu erstellen
-     */
-
-    public ResponseEntity<?> registration(ExternalRequest externalRequest) {
-
-        External external = new External(
-                externalRequest.getFirstName(),
-                externalRequest.getLastName(),
-                externalRequest.getEmail(),
-                externalRequest.getPassword(),
-                null,
-                Role.EXTERN,
-                false,
-                true,
-                externalRequest.getCompany(),
-                externalRequest.getAvailabilityStart(),
-                externalRequest.getAvailabilityEnd(),
-                externalRequest.getDescription(),
-                null  // Set BachelorSubjects to null initially
-        );
-
-        // Save External without BachelorSubjects
-        External savedExternal = externalRepository.save(external);
-
-        // Now handle BachelorSubjects
-        List<BachelorSubject> bachelorSubjects = externalRequest.getBachelorSubjects();
-        if (bachelorSubjects != null) {
-            for (BachelorSubject subject : bachelorSubjects) {
-                // Set the External entity for each BachelorSubject
-                subject.setExternal(savedExternal);
+            // BachelorSubjects behandeln
+            List<BachelorSubject> bachelorSubjects = externalRequest.getBachelorSubjects();
+            if (bachelorSubjects != null) {
+                for (BachelorSubject subject : bachelorSubjects) {
+                    // Set the External entity for each BachelorSubject
+                    subject.setExternal(savedExternal);
+                }
+                // dazugehörige BachelorSubjects speichern
+                savedExternal.setBachelorSubjects(bachelorSubjectRepository.saveAll(bachelorSubjects));
             }
-            // Save the associated BachelorSubjects
-            savedExternal.setBachelorSubjects(bachelorSubjectRepository.saveAll(bachelorSubjects));
+
+
+            return ResponseEntity.ok(savedExternal);
+        } catch (RuntimeException e) {
+            return ResponseEntity.status(400).body(e.getMessage());
         }
-
-        return ResponseEntity.ok(savedExternal);
-
-
-        /*
-        return registerExtern(new External(
-                        externalRequest.getFirstName(),
-                        externalRequest.getLastName(),
-                        externalRequest.getEmail(),
-                        externalRequest.getPassword(),
-                        null,
-                        Role.EXTERN,
-                        false,
-                        true,
-                        externalRequest.getCompany(),
-                        externalRequest.getAvailabilityStart(),
-                        externalRequest.getAvailabilityEnd(),
-                        externalRequest.getDescription(),
-                        externalRequest.getBachelorSubjects()
-                )
-        );*/
     }
-
 
     // Methode um Externe nach id zu laden
     public External getExternal(Long id) {
