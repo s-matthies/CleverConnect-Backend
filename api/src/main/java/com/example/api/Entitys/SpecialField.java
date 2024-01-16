cc12b4594702aec839689e9554ed70b0d2e389d9
nicole  driebe <blongsch@nicoles-mbp-2.fritz.box.remote.htw-berlin.de>
2024-01-16 08:29:01 +0100

    bei SpecialField Entity Id GenartionType zu IDENTY geaendert

--- api/src/main/java/com/example/api/Entitys/SpecialField.java
+++ api/src/main/java/com/example/api/Entitys/SpecialField.java
@@ -11,7 +11,7 @@ import java.util.List;
 public class SpecialField {
 
    @Id
-   @GeneratedValue(strategy = GenerationType.AUTO)
+   @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String value;
