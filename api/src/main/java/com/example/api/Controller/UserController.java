ca702ab838f606479e22152ec48af569eea66dec
Sabine Matthies <Sabine.Matthies@student.htw-berlin.de>
2024-01-31 17:50:40 +0100

    Javadoc-Kommentare bei UserController und AuthenticationService

--- api/src/main/java/com/example/api/Controller/UserController.java
+++ api/src/main/java/com/example/api/Controller/UserController.java
@@ -18,20 +18,34 @@ import java.util.List;
 
 // Controller verbindung zum Client, hier werden die methoden ausgeführt
 
+/**
+ * Controller für die Benutzer*innenverwaltung.
+ * Dieser Controller stellt Endpunkte zur Verfügung, um Benutzer*innen zu registrieren, zu authentifizieren,
+ * zu laden, zu aktualisieren, zu löschen und das Passwort zu ändern.
+ */
 @RestController
-// Pfad wird selbst festgelegt?
 @RequestMapping(path ="user")
 public class UserController {
-    // mit Service verknüpfen/verbinden
+
     @Autowired
     private final UserService userService;
     private final AuthenticationService service;
 
+    /**
+     * Konstruktor der Klasse UserController.
+     * @param userService Das UserService-Objekt für den Zugriff auf Benutzer*innendaten
+     * @param service AuthenticationService für die Authentifizierung von Benutzer*innen
+     */
     public UserController(UserService userService, AuthenticationService service) {
         this.userService = userService;
         this.service = service;
     }
 
+    /**
+     * Methode für die Registrierung einer Benutzer*in.
+     * @param userRequest Die Anfrage mit den Daten der zu registrierenden Benutzer*in
+     * @return ResponseEntity mit den Daten der registrierten Benutzer*in
+     */
     @PostMapping(path ="/register")
     //es wird angegeben, was man von Client Seite haben möchte - wir bekommen "Körper" vom Client
     // alle Attribute die im UserRequest sind, werden übergeben
@@ -39,61 +53,83 @@ public class UserController {
         return userService.register(userRequest);
     }
 
-
+    /**
+     * Methode für die Authentifizierung einer Benutzer*in.
+     * @param request Die Anfrage mit den Daten der zu authentifizierenden Benutzer*in
+     * @return ResponseEntity mit dem generierten JWT-Token
+     */
     @PostMapping(path ="/authenticate")
     public ResponseEntity<AuthenticationResponse> register(
             @RequestBody AuthenticationRequest request){
         return ResponseEntity.ok(service.authenticate(request));
     }
 
-
+    /**
+     * Methode für das Laden aller Benutzer*innen.
+     * @return ResponseEntity mit den Daten aller Benutzer*innen
+     */
     @PostMapping("/login")
     public ResponseEntity<Object> signInUser(@RequestBody LoginRequest loginRequest) {
         return userService.signInUser(loginRequest.getEmail(), loginRequest.getPassword());
     }
 
-    //alle User laden
+    /**
+     * Methode für das Laden aller Benutzer*innen.
+     * @return ResponseEntity mit den Daten aller Benutzer*innen
+     */
     @GetMapping("/load")
     List<User> allUsers() {
         return userService.allUser();
     }
 
-    //einzelne User mit ID laden
+    /**
+     * Lädt eine*n bestimmte*n Benutzer*in.
+     * @param id Die ID der Benutzer*in
+     * @return ResponseEntity mit den Daten der Benutzer*in
+     */
     @GetMapping("/load/{id}")
     User getUsers(@PathVariable Long id) {
         return userService.getUser(id);
     }
 
-    // Daten eines Users ändern
     /**
-     * Aktualisiert einen Benutzer mit den bereitgestellten Informationen.
-     * Diese Methode ist dafür verantwortlich, einen vorhandenen Benutzer mit der angegebenen
-     * ID mithilfe der im Parameter {@code newUser} bereitgestellten Informationen zu aktualisieren.
-     * Die Methode gibt ein ResponseEntity zurück, das die aktualisierten Benutzerinformationen im Erfolgsfall
-     * oder eine JSON-Antwort mit einer Fehlermeldung im Fehlerfall enthält.
-     *
-     * @param id Die eindeutige Kennung des zu aktualisierenden Benutzers.
-     * @param newUser Das User-Objekt, das die aktualisierten Informationen enthält.
-     * @return ResponseEntity mit dem HTTP-Status 200 OK und den aktualisierten Benutzerinformationen im Erfolgsfall,
-     *         oder ein ResponseEntity mit dem HTTP-Status 400 BAD REQUEST und einer Fehlermeldung im JSON-Format im Fehlerfall.
+     * Aktualisiert einen Benutzer*in.
+     * @param id Die ID des Benutzers*in
+     * @param newUser Das User-Objekt, das die aktualisierten Informationen enthält
+     * @return ResponseEntity mit den aktualisierten Benutzerinformationen
      */
     @PutMapping("/update/{id}")
     public ResponseEntity<User> updateUser (@PathVariable Long id, @RequestBody User newUser) {
         return userService.updateUser(id, newUser);
     }
 
-    // eine Userin löschen
+    /**
+     * Löscht eine*n Benutzer*in.
+     * @param id Die ID der Benutzerin
+     * @return ResponseEntity mit den Daten der gelöschten Benutzer*in
+     */
     @DeleteMapping("/delete/{id}")
     public ResponseEntity<Object> deleteUser(@PathVariable Long id) {
         return userService.deleteUser(id);
     }
 
-
+    /**
+     * Methode für das Ausloggen einer Benutzer*in.
+     * @param request Die Anfrage mit den Daten der zu ausloggenden Benutzer*in
+     * @param response Die Antwort mit den Daten der zu ausloggenden Benutzer*in
+     * @return ResponseEntity mit den Daten der ausgeloggten Benutzer*in
+     */
     @GetMapping("/logout")
     public ResponseEntity<Object> logout(HttpServletRequest request, HttpServletResponse response) {
         return userService.signOut(request, response);
     }
 
+    /**
+     * Methode für das Ändern des Passworts einer Benutzer*in.
+     * @param token Das JWT-Token der Benutzer*in
+     * @param request Die Anfrage mit den Daten der zu ändernden Benutzer*in
+     * @return ResponseEntity mit den Daten der geänderten Benutzer*in
+     */
     @PutMapping("/updatePassword/{id}")
     public ResponseEntity<Object> updatePassword(@RequestHeader("Authorization") String token,
                                                  @RequestBody PasswordChangeRequest request) {
--- api/src/main/java/com/example/api/Security/auth/AuthenticationService.java
+++ api/src/main/java/com/example/api/Security/auth/AuthenticationService.java
@@ -6,14 +6,22 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.stereotype.Service;
 
+
+/**
+ * Service für die Authentifizierung von Benutzer*innen.
+ */
 @Service
 public class AuthenticationService {
-
     private  final UserRepository repository;
     private final AuthenticationManager authenticationManager;
     private final JwtService JwtService;
 
-
+    /**
+     * Konstruktor der Klasse AuthenticationService.
+     * @param repository Das UserRepository-Objekt für den Zugriff auf Benutzer*innendaten
+     * @param authenticationManager AuthenticationManager für die Authentifizierung von Benutzer*innen
+     * @param jwtService JwtService für die Erstellung von JWT-Token
+     */
     public AuthenticationService(UserRepository repository,
                                  AuthenticationManager authenticationManager,
                                  JwtService jwtService) {
@@ -22,6 +30,12 @@ public class AuthenticationService {
         this.JwtService = jwtService;
     }
 
+    /**
+     * Authentifiziert eine Benutzer*in und gibt ein JWT-Token zurück.
+     * @param request Die Anfrage mit den Daten der zu authentifizierenden Benutzer*in (mit E-Mail-Adresse und Passwort)
+     * @return AuthenticationResponse mit dem generierten JWT-Token
+     * @throws Exception wenn die Authentifizierung fehlschlägt oder die Benutzer*in nicht gefunden wird
+     */
     public AuthenticationResponse authenticate(AuthenticationRequest request) {
 
         authenticationManager.authenticate(
