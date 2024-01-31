c2785d854a5ab4e94c470bc64cf662345603d8fe
Sabine Matthies <Sabine.Matthies@student.htw-berlin.de>
2024-01-31 16:55:28 +0100

    Update UserConfig: Passwort wird verschlüsselt

--- api/src/main/java/com/example/api/Config/UserConfig.java
+++ api/src/main/java/com/example/api/Config/UserConfig.java
@@ -7,64 +7,59 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import java.time.LocalDate;
 import java.util.List;
 
+
 @Configuration
 public class UserConfig {
+
+/*
 
     // wir verknüpfen mit dem UserRepository
     @Autowired
     private final UserRepository userRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
-    public UserConfig(UserRepository userRepository) {
+    public UserConfig(UserRepository userRepository, BCryptPasswordEncoder bCryptPasswordEncoder) {
         this.userRepository = userRepository;
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
     }
-/*
+
     //wird beim Start des Programms ausgeführt
-    /*
+
     @Bean
     CommandLineRunner commandLineRunner(){
         return args -> {
-            User user1 = new User(
-                    "Anna",
-                    "Bertram",
-                    "meineMail@Student.HTW-Berlin.de",
-                    "12345-Hundert",
-                    LocalDate.of(2023, 10, 1),
-                    Role.STUDENT,
-                    false,
-                    true
-            );
 
-            User user2 = new User(
-                    "Jörn",
+            User user1 = new User(
+                    "Joern",
                     "Freiheit",
                     "Joern.Freiheit@HTW-Berlin.de",
-                    "passwort_Aber_sicher!",
-                    LocalDate.of(2023, 10, 15),
+                    bCryptPasswordEncoder.encode("passwort_Aber_sicher5!"),                    LocalDate.of(2023, 10, 15),
                     Role.ADMIN,
                     false,
                     true
             );
 
-            User user3 = new User(
+            User user2 = new User(
                     "Helena",
                     "Mihaljevic",
                     "Helena.Mihaljevic@HTW-Berlin.de",
-                    "passwort123#",
-                    LocalDate.of(2023, 11, 1),
+                    bCryptPasswordEncoder.encode("Passwort123#"),                    LocalDate.of(2023, 11, 1),
                     Role.ADMIN,
                     false,
                     true
             );
 
             userRepository.saveAll(
-                    List.of(user1, user2, user3)
+                    List.of(user1, user2)
             );
         };
     }
+    
+ */
 
-     */
 }
