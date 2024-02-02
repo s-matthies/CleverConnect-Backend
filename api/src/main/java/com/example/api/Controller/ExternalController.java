b8d9faf9c190b93b78826137a05394c8daf04fb1
Sabine Matthies <Sabine.Matthies@student.htw-berlin.de>
2024-02-02 19:58:53 +0100

    Update: Javadoc ExternalController

--- api/src/main/java/com/example/api/Controller/ExternalController.java
+++ api/src/main/java/com/example/api/Controller/ExternalController.java
@@ -1,8 +1,6 @@
 package com.example.api.Controller;
 
 import com.example.api.DTO.ExternalDTO;
-import com.example.api.Entitys.BachelorSubject;
-import com.example.api.Entitys.External;
 import com.example.api.Request.ExternalRequest;
 import com.example.api.Service.ExternalService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,22 +9,29 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+/**
+ * Controller für die externen Personen (Zweitbetreuer*innen)
+ */
 @RestController
 @RequestMapping("/external")
 public class ExternalController {
     @Autowired
-    //mit dem Service verbinden
     private final ExternalService externalService;
 
+    /**
+     * Konstruktor für den ExternalController
+     * @param externalService Der Service für die externen Personen (Zweitbetreuer*innen)
+     */
     public ExternalController(ExternalService externalService) {
         this.externalService = externalService;
     }
 
 
     /**
-     * Methode für die Registrierung einer externen Person (Zweitbtreuer*in)
-     * @param externalRequest Die Anfrage mit den Daten der zu registrierenden Person (Zweitbetreuer*in)
-     * @return ResponseEntity mit den Daten der registrierten Person (Zweitbetreuer*in)
+     * Methode für das Registrieren einer externen Person (Zweitbetreuer*in)
+     *
+     * @param externalRequest Die Daten der externen Person (Zweitbetreuer*in)
+     * @return ResponseEntity mit den Daten der registrierten externen Person (Zweitbetreuer*in)
      */
     @PostMapping("/register")
     public ResponseEntity<Object> register(@RequestBody ExternalRequest externalRequest) {
@@ -35,6 +40,7 @@ public class ExternalController {
 
     /**
      * Methode für das Laden aller externen Personen (Zweitbetreuer*innen)
+     *
      * @return ResponseEntity mit den Daten aller externen Personen (Zweitbetreuer*innen)
      */
     @GetMapping("/load")
@@ -44,7 +50,8 @@ public class ExternalController {
     }
 
     /**
-     * Methode für das Laden einer externen Person (Zweitbetreuer*in)
+     * Methode für das Laden einer externen Person (Zweitbetreuer*in) per ID
+     *
      * @param id Die ID der externen Person (Zweitbetreuer*in)
      * @return ResponseEntity mit den Daten der externen Person (Zweitbetreuer*in)
      */
@@ -55,7 +62,8 @@ public class ExternalController {
     }
 
     /**
-     * Methode für das Laden einer externen Person (Zweitbetreuer*in)
+     * Methode für das Laden einer externen Person (Zweitbetreuer*in) per E-Mail
+     *
      * @param email Die E-Mail-Adresse der externen Person (Zweitbetreuer*in)
      * @return ResponseEntity mit den Daten der externen Person (Zweitbetreuer*in)
      */
@@ -66,9 +74,11 @@ public class ExternalController {
     }
 
     /**
-     * Methode für das Löschen einer externen Person (Zweitbetreuer*in)
+     * Methode für das Löschen einer externen Person (Zweitbetreuer*in) per ID
+     *
      * @param id Die ID der externen Person (Zweitbetreuer*in)
-     * @return ResponseEntity mit den Daten der gelöschten externen Person (Zweitbetreuer*in)
+     * @param externalRequest Die Daten der externen Person (Zweitbetreuer*in)
+     * @return ResponseEntity mit einer Bestätigung
      */
     @PutMapping("/update/{id}")
     public ResponseEntity<Object> updateExternal (@PathVariable Long id,
