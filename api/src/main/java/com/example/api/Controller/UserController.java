ab0f029e1fe2c08b0f62d7d2a58f1fcb83058916
Sabine Matthies <Sabine.Matthies@student.htw-berlin.de>
2024-02-02 19:33:24 +0100

    Javadoc Dokumentation zum UserController hinzugefuegt

--- api/src/main/java/com/example/api/Controller/UserController.java
+++ api/src/main/java/com/example/api/Controller/UserController.java
@@ -5,8 +5,6 @@ import com.example.api.Entitys.User;
 import com.example.api.Request.LoginRequest;
 import com.example.api.Request.PasswordChangeRequest;
 import com.example.api.Request.UserRequest;
-import com.example.api.Security.auth.AuthenticationRequest;
-import com.example.api.Security.auth.AuthenticationResponse;
 import com.example.api.Security.auth.AuthenticationService;
 import com.example.api.Service.UserService;
 import jakarta.servlet.http.HttpServletRequest;
@@ -17,25 +15,26 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-// Controller verbindung zum Client, hier werden die methoden ausgeführt
-
 /**
- * Controller für die Benutzer*innenverwaltung.
- * Dieser Controller stellt Endpunkte zur Verfügung, um Benutzer*innen zu registrieren, zu authentifizieren,
- * zu laden, zu aktualisieren, zu löschen und das Passwort zu ändern.
+ * Die Klasse UserController ist für die Verwaltung von Usern verantwortlich.
+ * Sie bietet Methoden zum Registrieren, Einloggen, Ausloggen, Laden, Aktualisieren und Löschen von Benutzer*innen.
+ * Sie bietet auch eine Methode zum Ändern des Passworts von Benutzer*innen.
  */
 @RestController
 @RequestMapping(path ="user")
 public class UserController {
+
 
     @Autowired
     private final UserService userService;
     private final AuthenticationService service;
 
     /**
-     * Konstruktor der Klasse UserController.
-     * @param userService Das UserService-Objekt für den Zugriff auf Benutzer*innendaten
-     * @param service AuthenticationService für die Authentifizierung von Benutzer*innen
+     * Konstruktor für die Klasse UserController.
+     * Initialisiert den UserController mit einem UserService und einem AuthenticationService.
+     *
+     * @param userService Der Service für die Benutzer*innenverwaltung
+     * @param service Der Service für die Authentifizierung
      */
     public UserController(UserService userService, AuthenticationService service) {
         this.userService = userService;
@@ -43,30 +42,22 @@ public class UserController {
     }
 
     /**
-     * Methode für die Registrierung einer Benutzer*in.
-     * @param userRequest Die Anfrage mit den Daten der zu registrierenden Benutzer*in
-     * @return ResponseEntity mit den Daten der registrierten Benutzer*in
+     * Methode für die Registrierung eines Users.
+     * Nimmt eine UserRequest entgegen und delegiert die Registrierung an den UserService.
+     *
+     * @param userRequest Die Anfrage mit den Daten des zu registrierenden Users
+     * @return ResponseEntity mit den Daten des registrierten Users
      */
     @PostMapping(path ="/register")
-    //es wird angegeben, was man von Client Seite haben möchte - wir bekommen "Körper" vom Client
-    // alle Attribute die im UserRequest sind, werden übergeben
     public ResponseEntity<Object> register(@RequestBody UserRequest userRequest){
         return userService.register(userRequest);
     }
 
-    //die Methode ist redundant, da wir die Methode authenticate in AuthenticationService haben
-    /*
-    @PostMapping(path ="/authenticate")
-    public ResponseEntity<AuthenticationResponse> register(
-            @RequestBody AuthenticationRequest request){
-        return ResponseEntity.ok(service.authenticate(request));
-    }
-    */
-
-    /**
-     * Methode für das Einloggen einer Benutzer*in.
-     * @param loginRequest Die Anfrage mit den Daten der zu einloggenden Benutzer*in
-     * @return ResponseEntity mit den Daten der eingeloggten Benutzer*in
+    /** Methode für das Einloggen eines Users.
+     * Nimmt eine LoginRequest entgegen und delegiert das Einloggen an den UserService.
+     *
+     * @param loginRequest Die Anfrage mit den Daten des zu einloggenden Users
+     * @return ResponseEntity mit den Daten des eingeloggten Users
      */
     @PostMapping("/login")
     public ResponseEntity<SignInResponse> signInUser(@RequestBody LoginRequest loginRequest) {
@@ -74,8 +65,10 @@ public class UserController {
     }
 
     /**
-     * Methode für das Laden aller Benutzer*innen.
-     * @return ResponseEntity mit den Daten aller Benutzer*innen
+     * Methode für das Laden aller User.
+     * Delegiert das Laden aller User an den UserService.
+     *
+     * @return ResponseEntity mit den Daten aller User
      */
     @GetMapping("/load")
     List<User> allUsers() {
@@ -83,9 +76,11 @@ public class UserController {
     }
 
     /**
-     * Lädt eine*n bestimmte*n Benutzer*in.
-     * @param id Die ID der Benutzer*in
-     * @return ResponseEntity mit den Daten der Benutzer*in
+     * Methode für das Laden eines Users.
+     * Delegiert das Laden eines Users an den UserService.
+     *
+     * @param id Die ID des Users
+     * @return ResponseEntity mit den Daten des Users
      */
     @GetMapping("/load/{id}")
     User getUsers(@PathVariable Long id) {
@@ -93,10 +88,13 @@ public class UserController {
     }
 
     /**
-     * Aktualisiert einen Benutzer*in.
-     * @param id Die ID des Benutzers*in
+     * Aktualisiert einen User.
+     * Nimmt eine ID und ein User-Objekt mit den aktualisierten Informationen entgegen
+     * und delegiert das Aktualisieren an den UserService.
+     *
+     * @param id Die ID des Users
      * @param newUser Das User-Objekt, das die aktualisierten Informationen enthält
-     * @return ResponseEntity mit den aktualisierten Benutzerinformationen
+     * @return ResponseEntity mit den aktualisierten Benutzer*innen-Informationen
      */
     @PutMapping("/update/{id}")
     public ResponseEntity<User> updateUser (@PathVariable Long id, @RequestBody User newUser) {
@@ -104,9 +102,11 @@ public class UserController {
     }
 
     /**
-     * Löscht eine*n Benutzer*in.
-     * @param id Die ID der Benutzerin
-     * @return ResponseEntity mit den Daten der gelöschten Benutzer*in
+     * Löscht einen User.
+     * Nimmt eine ID entgegen und delegiert das Löschen des Users an den UserService.
+     *
+     * @param id Die ID des zu löschenden Users
+     * @return ResponseEntity mit den Daten des gelöschten Users
      */
     @DeleteMapping("/delete/{id}")
     public ResponseEntity<Object> deleteUser(@PathVariable Long id) {
@@ -114,10 +114,12 @@ public class UserController {
     }
 
     /**
-     * Methode für das Ausloggen einer Benutzer*in.
-     * @param request Die Anfrage mit den Daten der zu ausloggenden Benutzer*in
-     * @param response Die Antwort mit den Daten der zu ausloggenden Benutzer*in
-     * @return ResponseEntity mit den Daten der ausgeloggten Benutzer*in
+     * Methode für das Ausloggen eines Users.
+     * Nimmt eine HttpServletRequest und eine HttpServletResponse entgegen und delegiert das Ausloggen an den UserService.
+     *
+     * @param request Die Anfrage mit den Daten des zu ausloggenden Users
+     * @param response Die Antwort an den zu ausloggenden User
+     * @return ResponseEntity mit den Daten des ausgeloggten Users
      */
     @GetMapping("/logout")
     public ResponseEntity<Object> logout(HttpServletRequest request, HttpServletResponse response) {
@@ -125,10 +127,12 @@ public class UserController {
     }
 
     /**
-     * Methode für das Ändern des Passworts einer Benutzer*in.
+     * Methode für das Ändern des Passworts eines Users.
+     * Nimmt ein JWT-Token und eine PasswordChangeRequest entgegen und delegiert das Ändern des Passworts an den UserService.
+     *
      * @param token Das JWT-Token der Benutzer*in
-     * @param request Die Anfrage mit den Daten der zu ändernden Benutzer*in
-     * @return ResponseEntity mit den Daten der geänderten Benutzer*in
+     * @param request Die Anfrage mit den Daten des zu ändernden Users
+     * @return ResponseEntity mit den Daten des geänderten Users
      */
     @PutMapping("/updatePassword/{id}")
     public ResponseEntity<Object> updatePassword(@RequestHeader("Authorization") String token,
